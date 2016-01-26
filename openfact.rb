36ccbd6b2f1f42cb4d1328d9355542b7abf01aa7
tristan L <tristus@hotmail.com>
2016-01-26 21:05:29 +0000

    openfact.rb

---
+++ openfact.rb
@@ -0,0 +1,21 @@
+require 'open-uri'
+require 'json'
+
+loop do
+    
+puts "entrez un numero de code barre:"
+bar_code = gets.chomp
+
+
+url="http://fr.openfoodfacts.org/api/v0/produit/#{bar_code}.json"
+
+json = open(url).read
+
+data  = JSON.parse(json)
+
+
+
+puts data['product']['product_name']
+
+end
+
